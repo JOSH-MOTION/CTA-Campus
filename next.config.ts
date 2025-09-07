ef1eeff61a14521a72fb9e6de194e5dbd4e533b9
Joshua Doe <joshuadoe168@gmail.com>
2025-09-07 14:08:53 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,14 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  experimental: {
+    allowedDevOrigins: [
+      '**.' + process.env.GITPOD_WORKSPACE_ID + '.gitpod.io',
+      process.env.NEXT_PUBLIC_VERCEL_URL || '',
+      'http://localhost:3000',
+      'https://6000-firebase-studio-1753697219378.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev',
+    ].filter(Boolean),
+  }
 };
 
 export default nextConfig;
